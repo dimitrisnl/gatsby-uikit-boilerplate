893fb200baae42121587541db0a251da0c7398ef
Dimitrios Lytras <dnlytras@gmail.com>
2018-02-14 18:08:28 +0200

    testing window error

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import styled from 'react-emotion';
 
 import { Footer, Navigation } from 'Blocks';
+// import Icons from 'uikit/dist/js/uikit-icons.min';
 
-import Uikit from 'uikit/dist/js/uikit.min';
-import Icons from 'uikit/dist/js/uikit-icons.min';
-
-Uikit.use(Icons);
+// Uikit.use(Icons);
 
 import './styles.scss';
 
-const TemplateWrapper = ({ children }) => (
-  <Wrapper>
-    <Navigation />
-    <Main>{children()}</Main>
-    <Footer />
-  </Wrapper>
-);
+class TemplateWrapper extends Component {
+  componentDidMount() {
+    const isBrowser = typeof window !== 'undefined';
+    isBrowser && require('uikit');
+  }
+  render() {
+    return (
+      <Wrapper>
+        <Navigation />
+        <Main>{this.props.children()}</Main>
+        <Footer />
+      </Wrapper>
+    );
+  }
+}
+
+export default TemplateWrapper;
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
 };
-
-export default TemplateWrapper;
 
 const Wrapper = styled.div`
   min-height: 100vh;
